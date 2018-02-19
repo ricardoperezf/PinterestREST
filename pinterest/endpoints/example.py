9529ee06add54f2191e0dbf5a962243a1c89c070
ricardoperezf <ricardojpf98@gmail.com>
2018-02-19 14:15:32 -0600

    Added two examples of select and insert

--- pinterest/endpoints/example.py
+++ pinterest/endpoints/example.py
@@ -1,5 +1,30 @@
-from pinterest import pinterest_app
+from flask import jsonify
+from pinterest import pinterest_app, client
+
+db = client.ejemplo  # Select the database
+collection = db.notas
+
 
 @pinterest_app.route("/example")
-def example():
-    return "This is an example"
+def example_select_all_queries():
+    cursor = collection.find()
+    vector = []
+    for notas in cursor:
+        print(notas)
+        vector.append({
+            'nota': notas['nota'],
+            'materia': notas['materia']
+        })
+    return jsonify(vector)
+
+
+
+@pinterest_app.route("/add")
+def example_insert_query():
+    cursor = collection.insert(
+        {
+            'nota': 100,
+            'matera': 'cloud'
+        }
+    )
+    return jsonify("added")
